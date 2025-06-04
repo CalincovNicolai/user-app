a20da55e3f5f14745afc608963eb5d6558fce639
Calincov Nicolai <77538373+CalincovNicolai@users.noreply.github.com>
2025-06-04 04:14:25 +0300

    feat: adjust type page props

--- src/app/user/[uuid]/page.tsx
+++ src/app/user/[uuid]/page.tsx
@@ -3,13 +3,13 @@ import { fetchUsers } from '@/api/fetchUsers';
 import Link from 'next/link';
 import UserDetailsCard from '@/components/user/details/UserDetailsCard';
 
-type UserDetailPageParams = {
+type PageProps = {
   params: {
     uuid: string;
   };
 };
 
-export default async function UserDetailPage({ params }: UserDetailPageParams) {
+export default async function UserDetailPage({ params }: PageProps) {
   // Cannot get the specific user by id because the random api everytime randomizes the data on fetch
   // Also, cannot use the user list from localStorage because on server side it doesn't know about it
   // I'm just getting 1 user from api and display here
